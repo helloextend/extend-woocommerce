28ea1273f03f80956d7b743b0b13a19af05ce99f
Marko Crnkovic <marko777.ny@gmail.com>
2020-03-14 20:14:36 -0700

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-// The project is ready.
-console.log("running");
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
