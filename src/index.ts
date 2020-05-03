b6749bf5710f3afea1045e5aceb2f53daa57cdf1
Marko Crnkovic <marko777.ny@gmail.com>
2020-05-03 12:02:44 -0700

    refactored

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // USFCA 2020, Licensed to Extend
 /* START IMPORTS */
-import contractController from "./contracts.js";
+import contractController from './contracts.ts';
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -14,13 +14,17 @@ app.use(bodyParser.json());
 /* START SETUP */
 
 // Default Path
-app.get('/', (req: any, res: any) => res.send('Extend WooCommerce Integration Demo. Endpoint \'/\' does not do anything. Refer to documentation'));
+app.get('/', (req: any, res: any) =>
+  res.send("Extend WooCommerce Integration Demo. Endpoint '/' does not do anything. Refer to documentation"),
+);
 
 // Get Contract
 app.post('/contract', (req: any, res: any) => contractController.createContracts(req, res));
 
 // Get Endpoints
-app.get('/endpoints',  (req: any, res: any) => res.send('Current Endpoints<br/>----------------<br/>/contract<br/>---<br/>That\'s all. This may be incomplete.'));
-app.listen(PORT, () => console.log(`I'm all ears on ${ PORT }`))
+app.get('/endpoints', (req: any, res: any) =>
+  res.send("Current Endpoints<br/>----------------<br/>/contract<br/>---<br/>That's all. This may be incomplete."),
+);
+app.listen(PORT, () => console.log(`I'm all ears on ${PORT}`));
 
 /* END SETUP */
